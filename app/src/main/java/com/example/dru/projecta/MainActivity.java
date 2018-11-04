5494ccef0dfe0608d8c2b8af73c447b2e2311062
Droidallstars <droidallstars@gmail.com>
2018-11-03 23:10:31 -0400

    Declared TextView appName (main title text)

--- app/src/main/java/com/example/dru/projecta/MainActivity.java
+++ app/src/main/java/com/example/dru/projecta/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.dru.projecta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView appName;
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
